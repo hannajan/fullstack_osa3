07e8faeb865fe486724906703d5a355f9faac426
Hanna Jantunen <hanna.r.jantunen@helsinki.fi>
2019-10-25 15:13:02 +0300

    osa3.3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let persons = [
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
-    id: 4
+    id: 2
   },
   {
     name: "Dan Abramov",
@@ -39,6 +39,17 @@ let persons = [
       res.send(getInfo())
   })
   
+  app.get('/api/persons/:id', (req, res) => {
+      const id = Number(req.params.id)
+      const person = persons.find(person => person.id === id)
+
+      if(person) {
+      res.json(person)
+      } else {
+          res.status(404).end()
+      }
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
