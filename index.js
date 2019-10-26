c1ed418952103006603b6c034fdd2245c6b1ef44
Hanna Jantunen <hanna.r.jantunen@helsinki.fi>
2019-10-26 12:32:07 +0300

    herokua varten

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
@@ -102,7 +105,7 @@ let persons = [
       res.json(person)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
